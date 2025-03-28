5d559417bae94d0503871cdf949d25cb2ad8fcb7
BARKER, Kyle (Student) <kb477@students.waikato.ac.nz>
2025-03-29 12:06:35 +1300

    Commoities sorting and displaying in option drop down

--- 322-A2/getCommodities.php
+++ 322-A2/getCommodities.php
@@ -5,7 +5,7 @@
 include 'connectdb.php'; // Include the PDO connection
 
 // SQL query to get all events and thier id
-$sql = "SELECT `name` FROM commodities"; // SQL query to get all events
+$sql = "SELECT * FROM commodities"; // SQL query to get all events
 $result = $con->query($sql);
 
 $commoditiesList = []; // Store results in an array
--- 322-A2/index.js
+++ 322-A2/index.js
@@ -1,3 +1,4 @@
+let commodities = [];
 document.addEventListener("DOMContentLoaded", getCommoditiesList());
 
 async function getCommoditiesList() {
@@ -6,10 +7,33 @@ async function getCommoditiesList() {
     if (response.ok) {
       const data = await response.json();
       console.log(data);
+      commodities = data.map((commoditiy) => ({
+        id: commoditiy.id,
+        name: commoditiy.name,
+        information: commoditiy.information,
+        code: commoditiy.code,
+      }));
+      displayCommodities();
     } else {
       throw new Error("Failed to retrieve data");
     }
   } catch (error) {
     console.error("Error: ", error);
   }
+}
+
+function displayCommodities() {
+  // Sort Commodities
+  commodities.sort((a, b) => a.name.localeCompare(b.name));
+  select = document.getElementById("commodity-select");
+
+  select.innerHTML = "";
+
+  commodities.forEach((commoditiy) => {
+    const option = document.createElement("option");
+    option.value = commoditiy.id;
+    option.textContent = commoditiy.name;
+
+    select.appendChild(option);
+  });
 }
